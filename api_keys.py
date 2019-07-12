044ea12e2591c63be3965e7014c2f7ebe12852fc
rjm3q <robert.mabry86@gmail.com>
2019-07-12 06:57:33 -0500

    new

--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,6 @@
 # OpenWeatherMap API Key
-api_key = "YOUR KEY HERE!"
+wkey = "8617c28608099050ad2ada7a3ae52a24"
+
+gkey= 'AIzaSyAIK49iaMI_U-o2WEntWb5riZN5m14EnHI'
+
+census_key= '90a3b4413de14a5a52626999ba58589f7a37f90d'
